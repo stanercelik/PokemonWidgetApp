8263123d26ff8fea32ebd74b288f553116b31cbe
Taner Celik <tanercelik2001@gmail.com>
2023-05-08 17:52:20 +0300

    Initial Commit

---
+++ PokemonWidget/PokemonWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonWidgetApp.swift
+//  PokemonWidget
+//
+//  Created by Taner Çelik on 8.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct PokemonWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
